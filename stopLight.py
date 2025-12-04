e8427f74731f63fedc4e06caa496372c4c74ec56
thehogsriderg <oseir@nycstudents.net>
2025-12-04 11:56:57 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -4,21 +4,24 @@ from tkmacosx import Button
 
 # Create the main window
 root = Tk()
-root.title("Enter Title Here")
+root.title("thing")
 
 #Set size of window
 root.geometry("300x150")
 
 # Create buttons
 red_button = Button(root, text="Red", background='red')
+yellow_button = Button(root, text="yellow", background='yellow')
+green_button = Button(root, text="green", background='green')
 
 #Add a label
-label = Label(root, text="CHANGE ME!")
+label = Label(root, text="this was changed!")
 
 # Place widgets in window (with pack function!)
 label.pack()
 red_button.pack()
-
+yellow_button.pack()
+green_button.pack()
 
 # Start the GUI event loop
 root.mainloop()

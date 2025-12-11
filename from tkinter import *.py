f9264ed7153028299d0f05ec7d0d286dc1327472
thehogsriderg <oseir@nycstudents.net>
2025-12-11 12:00:41 -0500

    Create from tkinter import *.py

---
+++ from tkinter import *.py
@@ -0,0 +1,21 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+
+# Create the main window
+root = Tk()
+root.title('dihmaster')
+
+#Set size of window
+root.geometry("300x150")
+
+#Add a label
+label = Label(root, text="CHANGE ME!")
+
+# Place widgets in window (with pack function!)
+line(row1, )
+red_button.pack()
+
+
+# Start the GUI event loop
+root.mainloop()
